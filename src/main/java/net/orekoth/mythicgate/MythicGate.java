e611a6574fc31dee4eec23a3cc6bc4b36be51557
bigbluewave <50306080+bigbluewave@users.noreply.github.com>
2024-12-01 21:35:43 -0800

    Port to 1.20.1

--- src/main/java/net/orekoth/mythicgate/MythicGate.java
+++ src/main/java/net/orekoth/mythicgate/MythicGate.java
@@ -1,10 +1,19 @@
 package net.orekoth.mythicgate;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.orekoth.mythicgate.item.MythicItems;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import draylar.gateofbabylon.registry.GOBItems;
 
 public class MythicGate implements ModInitializer {
 	public static final String MOD_ID = "mythicgate";
@@ -14,12 +23,30 @@ public class MythicGate implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("modid");
 
+	public static final ItemGroup GROUP = FabricItemGroup.builder().displayName(Text.translatable("itemGroup.mythicgate.group"))
+	.icon(() -> new ItemStack(MythicItems.MYTHRIL_BROADSWORD))
+	.entries((context, entries) -> {
+		GOBItems.init();
+
+		// Add GOB registry to ItemGroup
+		Registries.ITEM.getEntrySet().stream().filter(entry -> entry.getKey().getValue().getNamespace().equals("mythicgate")).forEach(item -> {
+			entries.add(new ItemStack(item.getValue()));
+		});
+	})
+	.build();
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+		Registry.register(Registries.ITEM_GROUP, id("group"), GROUP);
+
 		MythicItems.registerModItems();
 	}
+
+	public static Identifier id(String name) {
+        return new Identifier(MythicGate.MOD_ID, name);
+    }
 }
